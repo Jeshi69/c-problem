c064a3a2bdd3adda2410f06f33f2e4bb87f65764
Jeshi69 <miftauljannatjeshi@gmail.com>
2025-07-30 09:10:46 +0600

    Initial commit

---
+++ F_Digits_Summation.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   long long int a,b;
+    cin >> a>> b;
+    a = a%10;
+    b = b%10;
+   long long int sum = a+b;
+    cout << sum <<endl;
+    return 0;
+    
+}
---
+++ G_Summation_from_1_to_N.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   long long int N;
+    long long int sum = 0;
+    cin >> N;
+     sum = (N * (N + 1)) / 2;
+ 
+    
+    cout << sum << endl;
+    return 0;
+}
